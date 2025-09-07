6af4be64d32e7d91df7ef09fb956d664319db5ee
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2025-09-07 15:59:17 +0530

    Time: 1 ms (98.84%) | Memory: 44.7 MB (96.59%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,51 +1,26 @@
-import java.util.*;
-
 class Solution {
-    public int[][] insert(int[][] interval, int[] newi) {
-        int a1 = newi[0], a2 = newi[1], n = interval.length;
-
-    
-        if (n == 0) return new int[][]{{a1, a2}};
-
-        HashSet<Integer> hs = new HashSet<>();
-
-     
-        for (int i = 0; i < n; i++) {
-            int b1 = interval[i][0], b2 = interval[i][1];
-            if (!(b2 < a1 || b1 > a2)) hs.add(i);
-        }
-
-        if (hs.isEmpty()) {
-            int insertPos = n; 
-            for (int i = 0; i < n; i++) {
-                if (interval[i][0] > a1) { insertPos = i; break; }
-            }
-            int[][] ans = new int[n + 1][2];
-            int idx = 0;
-            for (int i = 0; i < insertPos; i++) ans[idx++] = new int[]{interval[i][0], interval[i][1]};
-            ans[idx++] = new int[]{a1, a2};
-            for (int i = insertPos; i < n; i++) ans[idx++] = new int[]{interval[i][0], interval[i][1]};
-            return ans;
-        }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> out = new ArrayList<>();
+        int i = 0, n = intervals.length;
 
       
-        int first = Integer.MAX_VALUE, last = Integer.MIN_VALUE;
-        int mergedStart = a1, mergedEnd = a2;
-        for (int idx : hs) {
-            first = Math.min(first, idx);
-            last = Math.max(last, idx);
-            mergedStart = Math.min(mergedStart, interval[idx][0]);
-            mergedEnd = Math.max(mergedEnd, interval[idx][1]);
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            out.add(intervals[i++]);
         }
 
-        
-        int newSize = n - hs.size() + 1;
-        int[][] ans = new int[newSize][2];
-        int k = 0;
-        for (int i = 0; i < first; i++) ans[k++] = new int[]{interval[i][0], interval[i][1]};
-        ans[k++] = new int[]{mergedStart, mergedEnd};
-        for (int i = last + 1; i < n; i++) ans[k++] = new int[]{interval[i][0], interval[i][1]};
+       
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        out.add(newInterval);
 
-        return ans;
+  
+        while (i < n) {
+            out.add(intervals[i++]);
+        }
+
+        return out.toArray(new int[out.size()][]);
     }
 }
