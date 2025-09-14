1bd7acca36cc5e8a75ea39fb944b35c25f89a33d
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2025-09-14 11:41:59 +0530

    Time: 15 ms (46.44%) | Memory: 45.7 MB (56.01%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,31 +1,38 @@
+import java.util.*;
+
 class Solution {
     public String frequencySort(String s) {
-      
-        HashMap<Character,Integer> hm= new HashMap<>();
-        int n=s.length();
-         int arr[][]=new int[s.length()][3];
-         
-        for(int i=0;i<n;i++){
-            char c=s.charAt(i);
-            hm.put(c,hm.getOrDefault(c,0)+1);
-        }
-        for(int i=0;i<n;i++){
-            int a=s.charAt(i);
-             char c=s.charAt(i);
-             arr[i][0]=hm.get(c);
-            arr[i][1]=a;
-            arr[i][2]=i;
-        }
-        Arrays.sort(arr, (a, b) -> {
-    if (a[0] != b[0]) return Integer.compare(-a[0], -b[0]);
-    return Integer.compare(-a[1], -b[1]);
-});
+        int n = s.length();
+        if (n == 0) return "";
 
-         StringBuilder sb=new StringBuilder("");
-         for(int i=0;i<n;i++){
-            sb.append(s.charAt(arr[i][2]));
-         }
-return sb.toString();
-        
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        List<List<Character>> buckets = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) buckets.add(new ArrayList<>());
+
+        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
+            int f = e.getValue();
+            char c = e.getKey();
+            buckets.get(f).add(c);
+        }
+
+        StringBuilder sb = new StringBuilder(n);
+        for (int f = n; f >= 1; f--) {
+            List<Character> bucket = buckets.get(f);
+            if (bucket.isEmpty()) continue;
+            for (char c : bucket) {
+                for (int i = 0; i < f; i++) sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.frequencySort("loveleetcode"));
     }
 }
