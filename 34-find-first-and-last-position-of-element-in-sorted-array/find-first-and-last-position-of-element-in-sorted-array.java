a8b721be4c52d8002bc6f45c6245a36c4c2dd2e6
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2025-09-21 17:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (17.36%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,45 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        boolean found=false;
-        int n=nums.length,start=-1,end=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target&&found==false){
-                start=i;
-                end=i;
-                found=true;
-                continue;
 
-            }
-            if(nums[i]==target){
-                 end=i;
-
+    int binarySearch(int nums[], int t) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == t) {
+                return mid;
+            } else if (t < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
-        int ans[]=new int[2];
-        ans[0]=start;
-        ans[1]=end;
-        return ans;
+        return -1;
+    }
+
+    public int[] searchRange(int[] nums, int t) {
+        int[] ans = new int[2];
+        if (nums == null || nums.length == 0) {
+            ans[0] = -1;
+            ans[1] = -1;
+            return ans;
+        }
+
+        int s = binarySearch(nums, t);
+        if (s == -1) {             
+            return new int[]{-1, -1};
+        }
+
+        int i = s, j = s;
         
+        while (i + 1 < nums.length && nums[i + 1] == t) {
+            i++;
+        }
+       
+        while (j - 1 >= 0 && nums[j - 1] == t) {
+            j--;
+        }
+
+        ans[0] = j; 
+        ans[1] = i; 
+        return ans;
     }
 }
