80d6b2a99b33be2c833d6b4ac97af5b655273d57
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2025-12-20 18:32:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search insert position of K in a sorted array/search-insert-position-of-k-in-a-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsertK(int arr[], int k) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+            return i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>k)
+            return i;
+        }
+        return n;
+
+    }
+};
