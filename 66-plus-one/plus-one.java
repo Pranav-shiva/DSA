6b33b90013d28f75576e30de010fc7e09c8b6780
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2026-01-01 12:25:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (37.47%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,46 +1,14 @@
 class Solution {
-    public int[] plusOne(int[] d) {
-        int n=d.length,carry=0;
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(int i=n-1;i>0;i--){
-            int sum=0;
-            if(i==n-1){
-                sum=d[i]+1;
-            
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
             }
-            else{
-                sum=d[i]+carry;
-            }
-
-            if(sum>9){
-                arr.add(0);
-
-                carry=1;
-            }
-            else{
-                carry=0;
-                arr.add(sum);
-            }
-
-        }
-        int sum=d[0]+carry;
-        if(n==1){
-           sum= sum+1;
-        }
-        if(sum>9)
-        {
-            arr.add(0);
-            arr.add(1);
-        }
-        else {
-            arr.add(sum);
-        }
-        Collections.reverse(arr);
-        int ans[]=new int[arr.size()];
-        for(int i=0;i<arr.size();i++){
-            ans[i]=arr.get(i);
-        }
-        return ans;
-        
+            digits[i] = 0;
+        }   
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
