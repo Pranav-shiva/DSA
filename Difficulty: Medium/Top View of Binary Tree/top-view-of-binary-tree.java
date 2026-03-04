2079509b59bc647e1d61928b2e255b1cf4f1a680
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2026-03-04 23:30:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -13,35 +13,33 @@ class Node {
 class Solution {
     class pair{
         Node node;
-        int verticle;
-        public pair(Node node,int verticle){
+        int ver;
+        public pair(Node node,int ver){
             this.node=node;
-            this.verticle=verticle;
+            this.ver=ver;
             
         }
     }
     public ArrayList<Integer> topView(Node root) {
         // code here
-        ArrayList<Integer> ans=new ArrayList<>();
-        Map<Integer,Integer> map=new TreeMap<>();
+        ArrayList<Integer> arr=new ArrayList<>();
         Queue<pair> q=new LinkedList<>();
+        TreeMap<Integer,Integer> hm=new TreeMap<>();
         q.add(new pair(root,0));
         while(!q.isEmpty()){
             pair p=q.remove();
-            Node node=p.node;
-            int verticle=p.verticle;
-            if(!map.containsKey(verticle)){
-                map.put(verticle,node.data);
-            }
-            if(node.left!=null){
-                q.add(new pair(node.left,verticle-1));
-            }
-            if(node.right!=null){
-                q.add(new pair(node.right,verticle+1));
+            Node n=p.node;
+            
+            if(!hm.containsKey(p.ver))
+            hm.put(p.ver,n.data);
+            if(n.left!=null)
+            q.add(new pair(n.left,p.ver-1));
+            if(n.right!=null){
+                q.add(new pair(n.right,p.ver+1));
             }
         }
-        ans.addAll(map.values());
-        return ans;
+    arr.addAll(hm.values());
+    return arr;
         
     }
 }
