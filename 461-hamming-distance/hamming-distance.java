904f4a6d35efb22999d0a83767a5b88c622ce82c
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2026-03-23 08:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (34.41%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,12 +1,7 @@
 class Solution {
     public int hammingDistance(int z, int y) {
-        int x=z^y;
-        int c=0;
-        while(x!=0){
-            x=x&(x-1);
-            c++;
-        }
-    return c;
+     
+    return Integer.bitCount(z^y);
 
         
     }
