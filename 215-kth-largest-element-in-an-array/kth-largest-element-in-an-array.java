d1fbb8aad5bc8347b999fbd5cf24b719bfc7dc57
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2026-04-17 21:50:02 +0530

    Time: 66 ms (56.56%) | Memory: 68.5 MB (65.56%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -3,10 +3,10 @@ class Solution {
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int i=0;i<nums.length;i++){
             pq.add(nums[i]);
-            if(pq.size()>k){
-                pq.remove();
-            }
+            if(pq.size()>k)
+            pq.poll();
+
         }
         return pq.peek();
-    }
+}
 }
